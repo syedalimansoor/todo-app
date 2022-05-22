8df016aa867930efc18eb386b55bd4a9ffffbe40
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-22 18:14:26 +0500

    Create redux store

---
+++ src/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import mode from "@/slices/modeSlice";
+import todos from "@/slices/todosSlice";
+
+export default configureStore({
+  reducer: {
+    mode,
+    todos,
+  },
+});
