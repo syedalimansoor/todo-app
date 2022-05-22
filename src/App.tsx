fab2f13e6fdfa1f6f57ecc3606d01745dd24a9c1
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-23 00:34:14 +0500

    Create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import Page from "$/components/layout/Page";
 import { useSelector } from "react-redux";
-import { ThemeProvider } from "styled-components";
-import { modeSelector } from "./slices/modeSlice";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { modeSelector } from "$/slices/modeSlice";
 import themes from "$/themes";
+
+type ThemeProps = { theme: typeof themes["dark"] & typeof themes["light"] };
+const GlobalStyle = createGlobalStyle<ThemeProps>`
+:root {
+  font-family: ${(props) => props.theme.font.family};
+  font-size: ${(props) => props.theme.font.size};
+  font-weight: ${(props) => props.theme.font.weight.regular};
+  color: ${(props) => props.theme.text.secondary.default}
+}
+`;
 
 function App() {
   const mode = useSelector(modeSelector);
@@ -10,6 +20,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Page />
     </ThemeProvider>
   );
