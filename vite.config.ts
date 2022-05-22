dd480bc673f5e35f3a27dbad5304b65db75fbbcc
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-22 20:42:47 +0500

    Remove $components in favour of $/components
    For consistency

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       $: path.resolve(__dirname, "./src"),
-      $components: path.resolve(__dirname, "./src/components"),
     },
   },
 });
