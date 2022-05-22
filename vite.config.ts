31b58a9066b5e2ab424a4159dba58fa777b2100c
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-22 19:17:55 +0500

    remove path aliasing by @ in favour of $ for better intellisense

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      $: path.resolve(__dirname, "./src"),
+      $components: path.resolve(__dirname, "./src/components"),
     },
   },
 });
