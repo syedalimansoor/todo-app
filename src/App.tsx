65afb893fb386fed79e13e04499f96383e1cdb2f
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-22 21:38:18 +0500

    Retrieve theme and provide

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import Page from "$/components/layout/Page";
+import { useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { modeSelector } from "./slices/modeSlice";
+import themes from "$/themes";
 
 function App() {
-  return <Page />;
+  const mode = useSelector(modeSelector);
+  const theme = themes[mode];
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Page />
+    </ThemeProvider>
+  );
 }
 
 export default App;
