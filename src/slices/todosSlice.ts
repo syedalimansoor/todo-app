678a8caf61b5a595d8a2488366e47d138d270c67
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-22 17:06:27 +0500

    Add JSDoc for todoSlice reducers

--- src/slices/todosSlice.ts
+++ src/slices/todosSlice.ts
@@ -6,6 +6,7 @@ export const todosSlice = createSlice({
   name: "todos",
   initialState: [] as Todo[],
   reducers: {
+    /** Add a todo to the store */
     add: (state, action: { payload: { text: string } }) => {
       state.push({
         id: uuid(),
@@ -13,13 +14,19 @@ export const todosSlice = createSlice({
         text: action.payload.text,
       });
     },
+
+    /** Remove a todo from the store */
     remove: (state, action: { payload: { id: string } }) => {
       state = state.filter((todo) => todo.id !== action.payload.id);
     },
+
+    /** Edit an existing todo's text */
     edit: (state, action: { payload: { id: string; text: string } }) => {
       const todo = state.find((todo) => todo.id === action.payload.id);
       if (todo) todo.text = action.payload.text;
     },
+
+    /** Toggle an existing todo's status between active and completed */
     toggleStatus: (state, action: { payload: { id: string } }) => {
       const todo = state.find((todo) => todo.id === action.payload.id);
       if (todo) todo.status = todo.status === "active" ? "completed" : "active";
