f70298284b85dba92b36e9549eadc1a2a058dea0
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-22 21:11:40 +0500

    Export RootState and AppDispatch types

--- src/store.ts
+++ src/store.ts
@@ -2,9 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import mode from "$/slices/modeSlice";
 import todos from "$/slices/todosSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     mode,
     todos,
   },
 });
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
