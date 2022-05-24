3cfb3e41601789bd05bfc3ffde4e5c3dc359179a
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-25 02:54:52 +0500

    Create RemoveTodo

--- src/components/atomic/RemoveTodo.tsx
+++ src/components/atomic/RemoveTodo.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { FC, MouseEventHandler } from "react";
+import removeIcon from "$/assets/icon-cross.svg";
+import styled from "styled-components";
 
-const RemoveTodo = () => {
-  return <div>RemoveTodo</div>;
+interface Props {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  show?: boolean;
+}
+
+const Button = styled.button`
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  flex-shrink: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.8rem;
+`;
+
+const Image = styled.img<{ show: boolean }>`
+  opacity: ${(props) => (props.show ? 1 : 0)};
+  transition: 200ms ease;
+  width: 0.7rem;
+`;
+
+const RemoveTodo: FC<Props> = ({ onClick, show = true }) => {
+  return (
+    <Button onClick={show ? onClick : undefined}>
+      <Image src={removeIcon} alt="Remove todo" show={show} />
+    </Button>
+  );
 };
 
 export default RemoveTodo;
