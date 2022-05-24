085ada60d822c5b482b50a15da81c6f876703786
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-25 02:04:54 +0500

    Move TextWrapper to TodoText

--- src/components/composite/CreateTodo.tsx
+++ src/components/composite/CreateTodo.tsx
@@ -3,6 +3,7 @@ import RemoveTodo from "$/components/atomic/RemoveTodo";
 import TodoStatus from "$/components/atomic/TodoStatus";
 import TodoText from "$/components/atomic/TodoText";
 import styled from "styled-components";
+import { Status } from "$/types";
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,23 +12,17 @@ const Wrapper = styled.div`
   border-radius: 0.2rem;
 `;
 
-const TextAreaWrapper = styled.div`
-  flex-grow: 1;
-`;
-
 const CreateTodo = () => {
   const [text, setText] = useState("");
 
-  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (evt) => {
+  const handleTextChange: ChangeEventHandler<HTMLTextAreaElement> = (evt) => {
     setText(evt.target.value);
   };
 
   return (
     <Wrapper>
       <TodoStatus />
-      <TextAreaWrapper>
-        <TodoText text={text} onChange={handleChange} />
-      </TextAreaWrapper>
+      <TodoText text={text} onChange={handleTextChange} />
       <RemoveTodo />
     </Wrapper>
   );
