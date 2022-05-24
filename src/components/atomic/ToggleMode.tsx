827c06b6f2b11c3e0bf0a39be81554f5621cabc4
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-24 20:59:22 +0500

    Create ToggleMode

--- src/components/atomic/ToggleMode.tsx
+++ src/components/atomic/ToggleMode.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { modeSelector, toggle as toggleMode } from "$/slices/modeSlice";
+import { useAppDispatch, useAppSelector } from "$/hooks";
+import styled from "styled-components";
+
+import dark from "$/assets/icon-sun.svg";
+import light from "$/assets/icon-moon.svg";
+
+const images = { dark, light };
+const flip = { dark: "light", light: "dark" };
+
+const ToggleButton = styled.button`
+  display: flex;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+`;
 
 const ToggleMode = () => {
-  return <div>ToggleMode</div>;
+  const mode = useAppSelector(modeSelector);
+  const dispatch = useAppDispatch();
+
+  return (
+    <ToggleButton type="button" onClick={() => dispatch(toggleMode())}>
+      <img src={images[mode]} alt={`Switch to ${flip[mode]} mode`} />
+    </ToggleButton>
+  );
 };
 
 export default ToggleMode;
