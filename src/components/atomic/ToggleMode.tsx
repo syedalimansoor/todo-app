1aedebe5b335ee2a9e5d5b669b1a55366896d02b
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-25 23:08:13 +0500

    Animate mode icon on mode switch

--- src/components/atomic/ToggleMode.tsx
+++ src/components/atomic/ToggleMode.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { modeSelector, toggle as toggleMode } from "$/slices/modeSlice";
 import { useAppDispatch, useAppSelector } from "$/hooks";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import dark from "$/assets/icon-sun.svg";
 import light from "$/assets/icon-moon.svg";
+import { Mode } from "$/types";
 
 const images = { dark, light };
 const flip = { dark: "light", light: "dark" };
@@ -16,13 +17,38 @@ const ToggleButton = styled.button`
   cursor: pointer;
 `;
 
+const spin = keyframes`
+  from {
+    transform: rotate(-360deg)
+  } to {
+    transform: rotate(0)
+  }
+`;
+
+const Icon = styled.img<{ mode: Mode }>`
+  transition: transform 400ms cubic-bezier(0.34, 1.56, 0.64, 1);
+  transform: rotate(${(props) => (props.mode === "dark" ? "360" : "0")}deg);
+`;
+
 const ToggleMode = () => {
   const mode = useAppSelector(modeSelector);
   const dispatch = useAppDispatch();
 
+  // const icon = useRef<HTMLImageElement>(null);
+
+  // useEffect(() => {
+  //   if (icon.current) {
+  //     icon.current.style.animationPlayState = "running";
+  //   }
+  // }, [mode]);
+
   return (
     <ToggleButton type="button" onClick={() => dispatch(toggleMode())}>
-      <img src={images[mode]} alt={`Switch to ${flip[mode]} mode`} />
+      <Icon
+        src={images[mode]}
+        alt={`Switch to ${flip[mode]} mode`}
+        mode={mode}
+      />
     </ToggleButton>
   );
 };
