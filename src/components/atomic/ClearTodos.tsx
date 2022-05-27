aa11df12ba74fcc464f0dc2557d4b4e57493359c
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-27 13:20:47 +0500

    Create ClearTodos

--- src/components/atomic/ClearTodos.tsx
+++ src/components/atomic/ClearTodos.tsx
@@ -1,7 +1,25 @@
+import { removeCompleted } from "$/slices/todosSlice";
 import React from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border: none;
+  color: ${(props) => props.theme.text.secondary.default};
+  transition: 200ms ease;
+  cursor: pointer;
+
+  &:hover {
+    color: ${(props) => props.theme.text.secondary.hover};
+  }
+`;
 
 const ClearTodos = () => {
-  return <div>ClearTodos</div>;
+  const dispatch = useDispatch();
+  return (
+    <Button onClick={() => dispatch(removeCompleted())}>Clear completed</Button>
+  );
 };
 
 export default ClearTodos;
--- src/components/composite/TodoList.tsx
+++ src/components/composite/TodoList.tsx
@@ -1,17 +1,75 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import TodoItem from "$/components/composite/TodoItem";
 import TodosLeft from "$/components/atomic/TodosLeft";
 import FilterTodos from "$/components/atomic/FilterTodos";
 import ClearTodos from "$/components/atomic/ClearTodos";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { todosSelector } from "$/slices/todosSlice";
+import { Filter } from "$/types";
+import Sortable from "sortablejs";
+
+const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  position: relative;
+  background-color: ${(props) => props.theme.background.todo};
+  border-radius: 0.2em;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+  transition: background-color 200ms ease;
+`;
+
+const ListBody = styled.div``;
+
+const ListFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.7rem;
+  padding: 0.8rem;
+`;
+
+const FilterTodosWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 0.8rem;
+  position: absolute;
+  inset-block-start: calc(100% + 1rem);
+  inset-inline: 0;
+  border-radius: 0.2rem;
+  background-color: ${(props) => props.theme.background.todo};
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+  transition: background-color 200ms ease;
+`;
 
 const TodoList = () => {
+  const todos = useSelector(todosSelector);
+
+  const [filter, setFilter] = useState<Filter>("all");
+  const filteredTodos =
+    filter === "all" ? todos : todos.filter((todo) => todo.status === filter);
+
+  const listBody = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    listBody.current && new Sortable(listBody.current, { animation: 200 });
+  }, []);
+
   return (
-    <>
-      <TodoItem />
-      <TodosLeft />
-      <FilterTodos />
-      <ClearTodos />
-    </>
+    <ListWrapper>
+      <ListBody ref={listBody}>
+        {filteredTodos.map((todo) => (
+          <TodoItem key={todo.id} />
+        ))}
+      </ListBody>
+      <ListFooter>
+        <TodosLeft list={filteredTodos} />
+        <FilterTodosWrapper>
+          <FilterTodos filter={filter} setFilter={setFilter} />
+        </FilterTodosWrapper>
+        <ClearTodos />
+      </ListFooter>
+    </ListWrapper>
   );
 };
 
